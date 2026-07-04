603c476e64b7d5989919763a5ad7d4ff69692230
keerthanasangam <keerthanasangam0@gmail.com>
2026-07-04 18:45:35 +0530

    Day 2: Redesigned landing page

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+print(">>> Running backend/app.py <<<")
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Get the project root directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Create Flask app
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "frontend", "templates"),
+    static_folder=os.path.join(BASE_DIR, "frontend", "static")
+)
+#home route
 @app.route("/")
 def home():
-    return "<h1>🏥 Welcome to HealthHub AI</h1>"
+    print("✅ Home route executed")
+    return render_template("index.html")
 
+# Run the application
 if __name__ == "__main__":
     app.run(debug=True)
